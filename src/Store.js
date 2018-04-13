92192f5f5d6559abdbfea3fefc62acfb32a09dd4
Ulysse Hall <ulysse.hall@gmail.com>
2018-04-13 16:10:27 +0200

    Dynamisation page profile > ok

--- src/Store.js
+++ src/Store.js
@@ -13,19 +13,31 @@ export default new Vuex.Store({
         id: 1,
         name: 'Air Julie',
         nbNextSessions: 2,
-        archive: false
+        archive: false,
+        phone: '0666666666',
+        email: 'julie.air@gmail.com',
+        antecedent: 'Femme morte il y a 10ans , dans un accident de voiture',
+        fear: 'Voitures autonomes, maisons autonomes.'
       },
       {
         id: 2,
         name: 'Basser Philippine',
         nbNextSessions: 4,
-        archive: false
+        archive: false,
+        phone: '0677777777',
+        email: 'basser.philippine@gmail.com',
+        antecedent: 'Femme morte il y a 10ans , dans un accident de voiture',
+        fear: 'Voitures autonomes, maisons autonomes.'
       },
       {
         id: 3,
         name: 'Charrier Léa',
         nbNextSessions: 2,
-        archive: true
+        archive: true,
+        phone: '0888888888',
+        email: 'charrier.lea@gmail.com',
+        antecedent: 'Femme morte il y a 10ans , dans un accident de voiture',
+        fear: 'Voitures autonomes, maisons autonomes.'
       }
     ]
   },
